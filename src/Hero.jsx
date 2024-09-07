fa1346ccf6f7bbccefc0a8f967f26c48884092cf
Jari Leminaho <jaripiilola@gmail.com>
2024-09-07 19:26:02 +0200

    first commit

---
+++ src/Hero.jsx
@@ -0,0 +1,22 @@
+import heroImg from './assets/Hero.svg';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+            facilis perspiciatis nam quod. Eum excepturi quasi consequuntur
+            veritatis cum laudantium?
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
